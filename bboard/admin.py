332fd0d04d426c573578df7da526e3180e72e258
Grringoneer <grringo3@gmail.com>
2022-03-20 20:46:59 +0300

    add file by_rubric.htmt, which describes a new view for rubrics and navigating on them with bboard/number_rubrics

--- bboard/admin.py
+++ bboard/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Bb, Rubric
 
 class BbAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'price', 'published')
+    list_display = ('title', 'content', 'price', 'published', 'rubric')
     list_display_links = ('title', 'content')
     search_fields = ('title', 'content', )
 
