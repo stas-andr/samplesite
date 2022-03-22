74d9aa01f62a5177645d2fd1f644af93eeecb402
stasofob <64842680+stasofob@users.noreply.github.com>
2022-03-22 10:21:18 +0300

    add template html and edit index.html file

--- bboard/views.py
+++ bboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 from .models import Bb, Rubric
 from .forms import BbForm
@@ -21,7 +22,7 @@ def index(request):
 class BbCreateView(CreateView):
     template_name = 'bboard/create.html'
     form_class = BbForm
-    success_url = '/bboard/'
+    success_url = reverse_lazy('index')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
